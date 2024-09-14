ba25eb7f1b4846ce35f934ac5aee2ae8494851f3
Ludovic Manga <ludovicmanga@Ludovics-MacBook-Air.local>
2024-09-14 18:18:52 +0200

    auth done

--- front/src/components/NavBar/NavBar.tsx
+++ front/src/components/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import styles from "./NavBar.module.css";
 const NavBar = () => {
   return (
     <div className={styles.container}>
-      <div>Associe à impact</div>
+      <div>Associé à impact</div>
       <div>
         <div>Se connecter</div>
         <div>Inscription</div>
