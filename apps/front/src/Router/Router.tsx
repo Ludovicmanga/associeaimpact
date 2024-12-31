06dea65d0be611ffc29756628e7620ca9769a9e0
Ludovicmanga <ludovic.mangaj@gmail.com>
2024-12-31 13:08:53 +0100

    skeleton changed

--- apps/front/src/Router/Router.tsx
+++ apps/front/src/Router/Router.tsx
@@ -3,20 +3,22 @@ import {
   Navigate,
   RouterProvider,
 } from "react-router-dom";
-import { ProjectDetail } from "./pages/ProjectDetail/ProjectDetail";
-import Auth from "./pages/Auth/Auth";
-import ProjectsList from "./pages/ProjectsList/ProjectsList";
+import { ProjectDetail } from "../pages/ProjectDetail/ProjectDetail";
+import Auth from "../pages/Auth/Auth";
+import ProjectsList from "../pages/ProjectsList/ProjectsList";
 import { useEffect, useState } from "react";
-import { checkAuthApiCall } from "./helpers/auth.helper";
+import { checkAuthApiCall } from "../helpers/auth.helper";
 import { Alert, Skeleton, Snackbar } from "@mui/material";
-import { useAppDispatch, useAppSelector } from "./redux/hooks";
-import { setUser } from "./redux/userSlice";
-import ProjectCreation from "./pages/ProjectCreationOrEdition/ProjectCreationOrEdition";
-import Messages from "./pages/Messages/Messages";
-import StripeReturnPage from "./components/StripeReturnPage/StripeReturnPage";
-import { setSnackBar } from "./redux/snackbarSlice";
-import MyProfile from "./pages/MyProfile/MyProfile";
-import { VerifyEmail } from "./pages/VerifyEmail/VerifyEmail";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { setUser } from "../redux/userSlice";
+import ProjectCreation from "../pages/ProjectCreationOrEdition/ProjectCreationOrEdition";
+import Messages from "../pages/Messages/Messages";
+import StripeReturnPage from "../components/StripeReturnPage/StripeReturnPage";
+import { setSnackBar } from "../redux/snackbarSlice";
+import MyProfile from "../pages/MyProfile/MyProfile";
+import { VerifyEmail } from "../pages/VerifyEmail/VerifyEmail";
+import { ProjectsListSkeleton } from "../components/Skeletons/ProjectsListSkeleton/ProjectsListSkeleton";
+import styles from "./Router.module.css";
 
 const routesWhenLoggedIn = createBrowserRouter([
   {
@@ -143,7 +145,14 @@ const Router = () => {
       </Snackbar>
     </>
   ) : (
-    <Skeleton />
+    <div className={styles.skeletonContainer}>
+      <div className={styles.sideBarSkeleton}>
+        <Skeleton variant="rectangular" height={650} />
+      </div>
+      <div className={styles.mainContentSkeleton}>
+        <ProjectsListSkeleton />
+      </div>
+    </div>
   );
 };
 
